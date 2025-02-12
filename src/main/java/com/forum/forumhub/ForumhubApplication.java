9d6956dcb2d1b04c7c4b5045d14dd366ad89a12c
Nadi Duno <nadiduno@gmail.com>
2025-02-12 19:33:38 -0400

    Update dependecies

--- src/main/java/com/forum/forumhub/ForumhubApplication.java
+++ src/main/java/com/forum/forumhub/ForumhubApplication.java
@@ -8,6 +8,7 @@ public class ForumhubApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumhubApplication.class, args);
+		System.out.println("Run server");
 	}
 
 }
